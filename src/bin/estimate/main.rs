e800aaa75264f1527e145282dd5ad2c779a780fc
Dheshan Mohandass <dheshan@mohandass.com>
2024-05-25 16:49:38 -0400

    feat.: print largest 5 files using estimate
    Signed-off-by: Dheshan Mohandass <dheshan@mohandass.com>

--- src/bin/estimate/main.rs
+++ src/bin/estimate/main.rs
@@ -2,6 +2,8 @@ use clap::Parser;
 use sqlx::types::BigDecimal;
 use sqlx::Row;
 
+use db;
+
 #[derive(clap::Parser, Default, Debug)]
 #[clap(author = "Dheshan Mohandass", version, about)]
 /// A companion tool for the disk usage tracker to estimate a directory's size.
@@ -70,12 +72,72 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // NUMERIC type
     let total_size: BigDecimal = result.try_get("total_size")?;
+    let size_kb: BigDecimal = total_size.clone() / 1024;
+    let size_mb: BigDecimal = size_kb.clone() / 1024;
+    let size_gb: BigDecimal = size_mb.clone() / 1024;
+    let size_tb: BigDecimal = size_gb.clone() / 1024;
 
-    log::info!("Estimated size of directory:");
-    log::info!("{:.2} TB", total_size.clone() / 1024 / 1024 / 1024);
-    log::info!("{:.2} GB", total_size.clone() / 1024 / 1024);
-    log::info!("{:.2} MB", total_size.clone() / 1024);
-    log::info!("{:.2} KB", total_size);
+    log::info!("Estimated size: {:.2} TB = {:.2} GB = {:.2} MB = {:.2} KB = {:.2} bytes", size_tb, size_gb, size_mb, size_kb, total_size);
+
+    let largest_files_query = format!(
+        r#"
+        WITH RECURSIVE directory_tree AS (
+            SELECT d.directory_id
+            FROM directories d
+            WHERE d.directory_id = '{path}'
+
+            UNION ALL
+            
+            SELECT d.directory_id
+            FROM directories d
+            INNER JOIN directory_tree dt ON d.parent_id = dt.directory_id
+        )
+        SELECT f.file_id, f.size / 1024 / 1024 AS size_mb, f.owner_id, f.last_modified
+        FROM files f
+        WHERE f.directory_id IN (SELECT directory_id FROM directory_tree)
+        ORDER BY f.size DESC
+        LIMIT 5
+        "#,
+    );
+
+    let _ = tokio::task::spawn_blocking(move || {
+        let dfs = db::execute_queries::returning_df(&database_url, vec![&largest_files_query]);
+        if let Err(e) = dfs {
+            log::error!("Failed to return DataFrame: {}", e);
+            return;
+        }
+
+        let dfs = dfs.unwrap();
+        let df = &dfs[0];
+
+        // get col names
+        let mut col_names: Vec<&str> = vec![];
+        for series in df.get_columns() {
+            col_names.push(series.name());
+        }
+
+        let mut table = comfy_table::Table::new();
+        table.load_preset(comfy_table::presets::UTF8_FULL);
+        table.set_header(&col_names);
+
+        // iterate over the rows
+        let df_row_count = df.height();
+        for i in 0..df_row_count {
+            let mut row: Vec<String> = vec![];
+            for series in df.get_columns() {
+                let value: Result<polars::prelude::AnyValue, polars::prelude::PolarsError> = series.get(i);
+                let value_str = match value {
+                    Ok(value) => value.to_string(),
+                    Err(_) => "".to_string(),
+                };
+                row.push(value_str);
+            }
+            table.add_row(row);
+        }
+
+        println!("Largest files in directory:");
+        println!("{table}")
+    }).await?;
 
     Ok(())
 }
