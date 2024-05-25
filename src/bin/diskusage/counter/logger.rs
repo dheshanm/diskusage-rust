4078186f6a86f6166eb8bc25d61f3062fb98b491
Dheshan Mohandass <dheshan@mohandass.com>
2024-05-25 13:57:38 -0400

    chore: Move logger to separate module
    Signed-off-by: Dheshan Mohandass <dheshan@mohandass.com>

---
+++ src/bin/diskusage/counter/logger.rs
@@ -0,0 +1,56 @@
+use crate::models;
+use crate::models::definitions::DbModel;
+
+/// A function to log the disk usage statistics during runtime.
+/// Helps track the number of files and directories parsed per second.
+/// 
+/// Arguments
+/// * `handle` - A tokio runtime handle to run the async functions.
+/// * `pool` - A sqlx database pool to query the database.
+pub async fn logger_thread(handle: tokio::runtime::Handle, pool: sqlx::Pool<sqlx::Postgres>) {
+    std::thread::spawn(move || {
+        let log_frequency = match std::env::var("DISK_USAGE_LOG_FREQUENCY") {
+            Ok(frequency) => {
+                match frequency.parse::<u64>() {
+                    Ok(frequency) => {
+                        log::info!("Found log frequency from environment: {} seconds", frequency);
+                        std::time::Duration::from_secs(frequency)
+                    }
+                    Err(_) => {
+                        std::time::Duration::from_secs(300)
+                    }
+                }
+            }
+            Err(_) => {
+                std::time::Duration::from_secs(300)
+            }
+        };
+        log::info!("Logging thread: Active");
+        log::info!("Logging frequency: {:?}", log_frequency);
+        let mut files_counter = 0;
+        let mut directories_counter = 0;
+        loop {
+            let files_count = handle.block_on(async {
+                let result = models::definitions::File::count_all(&pool).await.unwrap_or_default();
+                result
+            });
+
+            let directories_count = handle.block_on(async {
+                let result = models::definitions::Directory::count_all(&pool).await.unwrap_or_default();
+                result
+            });
+
+            let files_count_diff = files_count - files_counter;
+            let directories_count_diff = directories_count - directories_counter;
+
+            files_counter = files_count;
+            directories_counter = directories_count;
+
+            let files_per_second = files_count_diff as f64 / log_frequency.as_secs() as f64;
+            let directories_per_second = directories_count_diff as f64 / log_frequency.as_secs() as f64;
+
+            log::info!("Parsed {} files ({} new) and {} directories ({} new): {:.2} files/s, {:.2} directories/s", files_count, files_count_diff, directories_count, directories_count_diff, files_per_second, directories_per_second);
+            std::thread::sleep(log_frequency);
+        }
+    });
+}
--- src/bin/diskusage/main.rs
+++ src/bin/diskusage/main.rs
@@ -1,6 +1,7 @@
 mod models;
 mod filesystem;
 mod users;
+mod counter;
 
 use clap::Parser;
 use rayon::prelude::*;
@@ -63,55 +64,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
 
     // Spawn a thread to query files processed periodically
-    let pool_c = pool.clone();
-    let handle_c = handle.clone();
-    std::thread::spawn(move || {
-        let log_frequency = match std::env::var("DISK_USAGE_LOG_FREQUENCY") {
-            Ok(frequency) => {
-                match frequency.parse::<u64>() {
-                    Ok(frequency) => {
-                        log::info!("Found log frequency from environment: {} seconds", frequency);
-                        std::time::Duration::from_secs(frequency)
-                    }
-                    Err(_) => {
-                        std::time::Duration::from_secs(300)
-                    }
-                }
-            }
-            Err(_) => {
-                std::time::Duration::from_secs(300)
-            }
-        };
-        log::info!("Logging thread: Active");
-        log::info!("Logging frequency: {:?}", log_frequency);
-        let mut files_counter = 0;
-        let mut directories_counter = 0;
-        loop {
-            let files_count = handle_c.block_on(async {
-                let result = models::definitions::File::count_all(&pool_c).await.unwrap_or_default();
-                result
-            });
-
-            let directories_count = handle_c.block_on(async {
-                let result = models::definitions::Directory::count_all(&pool_c).await.unwrap_or_default();
-                result
-            });
-
-            let files_count_diff = files_count - files_counter;
-            let directories_count_diff = directories_count - directories_counter;
-
-            files_counter = files_count;
-            directories_counter = directories_count;
-
-            let files_per_second = files_count_diff as f64 / log_frequency.as_secs() as f64;
-            let directories_per_second = directories_count_diff as f64 / log_frequency.as_secs() as f64;
-
-            log::info!("Parsed {} files ({} new) and {} directories ({} new)", files_count, files_count_diff, directories_count, directories_count_diff);
-            log::info!("{:.2} files/s, {:.2} directories/s", files_per_second, directories_per_second);
-            std::thread::sleep(log_frequency);
-        }
-    });
-
+    let pool_c: sqlx::Pool<sqlx::Postgres> = pool.clone();
+    let handle_c: tokio::runtime::Handle = handle.clone();
+    counter::logger::logger_thread(handle_c, pool_c).await;
+    
     log::info!("Starting disk usage tracking for: {}", root_dir);
     walkdir::WalkDir::new(root_dir)
         .into_iter()
